9fc3ef8696a3c756b5868a77a6e270489191d725
B-One <B-One@DESKTOP-4HF851F>
2022-04-23 11:27:28 -0700

    Add project files.

---
+++ Plugins.DataStore.SQL/Migrations/20220417201331_isSell.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Plugins.DataStore.SQL.Migrations
+{
+    public partial class isSell : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSell",
+                table: "Transactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSell",
+                table: "Transactions");
+        }
+    }
+}
